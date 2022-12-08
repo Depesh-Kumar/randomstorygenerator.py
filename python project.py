e829d26b24b348a04736648f9fe7b6b50ddeab51
Depesh kumar <118628585+Depesh-Kumar@users.noreply.github.com>
2022-12-08 14:31:48 +0530

    Add files via upload

---
+++ python project.py
@@ -0,0 +1,31 @@
+
+
+
+import random
+
+l1=["They met by chance. She was walking down the street, minding her own business, when she bumped into him. He apologized profusely, but she just smiled and said it was no big deal.They struck up a conversation and discovered that they had a lot in common. They talking for hours, until they finally exchanged numbers and went their separate ways."]
+l2=["They started dating shortly after that and they were very happy. They seemed to be perfect for each other. But then, out of nowhere, she broke up with him. She wouldn't give him a reason, she just said that she wasn't feeling it anymore. He was heartbroken, but he respected her decision."]
+l3=["A few months later, he ran into her again. They talked for a while and she eventually told him that she had only broken up with him because she was moving away and didn't want to drag him along. She still loved him and she was sorry for how she had handled things. They got back together and they were happier than ever.They got married a few years later and they are still together now. They are happy and they know that they are meant to be together. They are the perfect example of a love story."]
+l4=["Though they had been married for years, they still acted like newlyweds. He would bring her flowers for no reason and she would make him his favorite meals. They were truly in love.One day, she became ill and was diagnosed with a terminal illness. There was no cure. She didn't have long to live."]
+l5=["Though they had been married for years, they still couldn't help but feel like giddy teenagers whenever they were around each other. Even after all this time, they still couldn't get enough of each other. They were truly madly deeply in love.Every day, they found new ways to make each other laugh and feel loved. They were each other's best friend, confidante, and lover. They couldn't imagine life without the other and were grateful for every moment they got to spend together."]
+l6=["When they met, it was love at first sight. They knew they were meant to be together. They dated for a while and then got married. They were very happy together.They had their ups and downs like any other couple, but they always worked things out. They were truly in love.They had two beautiful children together and life was just perfect.Sadly, one day the man died suddenly. The woman was absolutely devastated. She didn't know how she was going to go on without him."]
+l7=["But she had to be strong for her children. She knew that their father would want her to be happy and to make the most of life.So she did just that. She lived her life to the fullest and made sure her children were happy.She always told them that their father was watching over them from heaven and that he was still with them in spirit.And she knew that he was.Though they sometimes took each other for granted, they knew that they were lucky to have found such a perfect match."] 
+l8=["They were truly blessed to have found each other and to be able to share their lives together.He was devastated. He refused to leave her side, determined to make her remaining days as happy as possible. He told her how much he loved her and how grateful he was to have her in his life.She passed away peacefully, surrounded by her husband's love."]
+n=random.randint(1, 9)
+if n==1:
+    print(l1)
+if n==2:
+    print(l2)
+if n==3:
+    print(l3)
+if n==4:
+    print(l4)
+if n==5:
+    print(l5)
+if n==6:
+    print(l6)
+if n==7:
+    print(l7)
+if n==8:
+    print(l8)
+
